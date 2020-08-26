5482a2b1a51f1227b0cba3a88833f1e8e9a920c0
TERO-ID <67523607+TERO-ID@users.noreply.github.com>
2020-08-26 23:17:25 +0300

    Add files via upload

---
+++ loadNS.py
@@ -0,0 +1,12 @@
+import os
+while True:
+  num =str(input())
+  if num == ('1') :
+    os.system(clear)
+    print('Загрузка NukeSpam...')
+      os.system(clear)
+      os.system(git clone https://github.com/TERO-ID/NukeSpam/)
+      os.system(cd NukeSpam)
+      os.system(pip install -r requirements.txt)
+    print('Запуск NukeSpam...')
+      os.system(python NukeSpam.py)
